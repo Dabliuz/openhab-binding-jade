c691623b794010ba5dd2b944fc91f1cf81eaed41
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2017-05-31 12:56:03 +0200

    some build adaptions

--- src/buildAndCopyToenflex-pi-defiant.sh
+++ src/buildAndCopyToenflex-pi-defiant.sh
@@ -3,3 +3,5 @@
 mvn clean package
 scp de.unidue.stud.sehawagn.openhab.binding.jade/target/de.unidue.stud.sehawagn.openhab.binding.jade-2.0.0-SNAPSHOT.jar enflex-pi-defiant:/opt/openhab/openhab-2.0.0.RC1/addons/
 scp de.unidue.stud.sehawagn.openhab.binding.jadeservice/target/de.unidue.stud.sehawagn.openhab.binding.jadeservice-2.0.0-SNAPSHOT.jar enflex-pi-defiant:/opt/openhab/openhab-2.0.0.RC1/addons/
+cp de.unidue.stud.sehawagn.openhab.binding.jade/target/de.unidue.stud.sehawagn.openhab.binding.jade-2.0.0-SNAPSHOT.jar /home/friesenkiwi/workspace/HyGrid/lib/AgentGui/projects/hygrid/lib/openHAB/
+
